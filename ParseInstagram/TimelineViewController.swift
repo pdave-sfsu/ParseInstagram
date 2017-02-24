2a00645cb610bf6284dda570844dea1b8ab41cd9
PD <dave.sfsu@gmail.com>
2017-02-24 15:34:05 -0800

    Added a feature to let the user logout

---
+++ ParseInstagram/TimelineViewController.swift
@@ -0,0 +1,49 @@
+//
+//  TimelineViewController.swift
+//  ParseInstagram
+//
+//  Created by Poojan Dave on 2/24/17.
+//  Copyright © 2017 Poojan Dave. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class TimelineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func logoutButtonPressed(_ sender: Any) {
+        
+        print("logout button pressed")
+        
+        PFUser.logOutInBackground { (error: Error?) in
+            if error == nil {
+                self.performSegue(withIdentifier: "logoutSegue", sender: nil)
+            }
+        }
+
+        
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
