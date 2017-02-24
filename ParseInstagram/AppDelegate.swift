b1c7653e836fbb947d327be4d3b582b5d330498b
PD <dave.sfsu@gmail.com>
2017-02-23 20:40:26 -0800

    Added the Heroku app to the progrma

--- ParseInstagram/AppDelegate.swift
+++ ParseInstagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration:ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "tempcodpath2"
+                configuration.clientKey = "thismaybeatest"  // set to nil assuming you have not set clientKey
+                configuration.server = "http://tempcodpath2.herokuapp.com/parse"
+            })
+        )
+        
         return true
     }
 
