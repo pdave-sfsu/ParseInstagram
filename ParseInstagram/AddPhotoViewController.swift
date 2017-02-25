206f6a3b89814ce113199f335b146edab1906d31
PD <dave.sfsu@gmail.com>
2017-02-24 16:19:47 -0800

    Added the ability to put photos into the app

--- ParseInstagram/AddPhotoViewController.swift
+++ ParseInstagram/AddPhotoViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class AddPhotoViewController: UIViewController {
+class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var addAPhotoButton: UIButton!
+    @IBOutlet weak var photoCaptionTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +27,36 @@ class AddPhotoViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        // Do something with the images (based on your use case)
+        
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func addAPhotoButtonPressed(_ sender: Any) {
+        
+        print("Add a photo button pressed")
+        
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        self.present(vc, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func submitButtonPressed(_ sender: Any) {
+        
+        print("Submit button pressed")
+        
+    }
 
     /*
     // MARK: - Navigation
