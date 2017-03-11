3be5a81be2a9b09d5158ce8ede67fe6237e25682
PD <dave.sfsu@gmail.com>
2017-03-10 21:09:53 -0800

    Cleaned up the instagramPost Cell; added comments

--- ParseInstagram/InstagramPost.swift
+++ ParseInstagram/InstagramPost.swift
@@ -7,52 +7,52 @@
 //
 
 import UIKit
-//import Parse and ParseUI
+// import Parse and ParseUI
 import Parse
 import ParseUI
 
-//cell for tableView
-//Each instagramPost has an imageView and a caption
+// cell for tableView
+// Each instagramPost has an imageView and a caption
 class InstagramPost: UITableViewCell {
 
-    //outlets
-    //pictureImageView is of type PFImageView, not UIImageView
+    // outlets
+    // pictureImageView is of type PFImageView, not UIImageView
     @IBOutlet weak var pictureImageView: PFImageView!
     @IBOutlet weak var captionLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     
-    //the PFObject being returned from Parse
-    //Needs to be deserialized
+    // the PFObject being returned from Parse
+    // Needs to be deserialized
     var instagramPost: PFObject! {
     
-        //sets the outlets with the approprate values
+        // sets the outlets with the approprate values
         didSet {
             
-            //retrieves the picture file and then loads the image
-            
+            // retrieves the picture file and then loads the image
             if let photo = instagramPost["actualPhoto"] {
+                // cast as a PFFile
                 self.pictureImageView.file = photo as? PFFile
             } else {
+                // Old key used for photo; cast ad PFFile
                 self.pictureImageView.file = instagramPost["media"] as? PFFile
             }
-
+            // loads the picture
             self.pictureImageView.loadInBackground()
             
-            //retrieves the caption and displays it
-            
+            // retrieves the caption and displays it
             if let pictureCaption = instagramPost["photoCaption"] {
                 self.captionLabel.text = pictureCaption as? String
             } else {
+                // Old key used for caption
                 self.captionLabel.text = instagramPost["caption"] as? String
             }
             
-            
-            
-            if let temp = instagramPost["date"] {
-                print(temp)
-                self.dateLabel.text = temp as? String
+            // retrieves the date and displays it
+            if let date = instagramPost["date"] {
+                self.dateLabel.text = date as? String
+                // If no date
             } else {
-                self.dateLabel.text = "Temp"
+                self.dateLabel.text = "No Date"
             }
             
         }
@@ -64,6 +64,7 @@ class InstagramPost: UITableViewCell {
         // Initialization code
     }
 
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
